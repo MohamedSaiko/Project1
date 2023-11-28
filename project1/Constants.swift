5a4a1b219c66b8988e70494daa28f84e809116fd
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-11-28 13:37:22 +0100

    add tableView data

---
+++ project1/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  project1
+//
+//  Created by Mohamed Sayed on 28.11.23.
+//
+
+import Foundation
+
+let pictureIdentifier = "Picture"
--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    
     var fileManager = Manager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         fileManager.manage()
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        fileManager.pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let pictureCell = tableView.dequeueReusableCell(withIdentifier: pictureIdentifier, for: indexPath)
+        var content = pictureCell.defaultContentConfiguration()
+        content.text = fileManager.pictures[indexPath.row]
+        pictureCell.contentConfiguration = content
+        return pictureCell
     }
 }
