3edeb5076a4afeaeeb934f049e3dfcfb25d86755
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-11-28 12:26:30 +0100

    add FileManager

---
+++ project1/Manager.swift
@@ -0,0 +1,31 @@
+//
+//  Manager.swift
+//  project1
+//
+//  Created by Mohamed Sayed on 28.11.23.
+//
+
+import Foundation
+
+struct Manager {
+    
+    var pictures = [String]()
+    
+    mutating func manage() {
+        let fm = FileManager.default
+        let wrappedpath = Bundle.main.resourcePath
+        guard let path = wrappedpath else {
+            return
+        }
+        let wrappeditems = try? fm.contentsOfDirectory(atPath: path)
+        guard let items = wrappeditems else {
+            return
+        }
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+}
--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var fileManager = Manager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fileManager.manage()
     }
 
 
