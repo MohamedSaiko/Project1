8b03493ab5d13884616a17e3d88a6c218b33dc5b
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-11-30 08:39:34 +0100

    Add Pictures Sharing Functionality

--- project1/Controllers/DetailViewController.swift
+++ project1/Controllers/DetailViewController.swift
@@ -18,6 +18,7 @@ class DetailViewController: UIViewController {
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -28,5 +29,14 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        activityViewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(activityViewController, animated: true)
     }
 }
